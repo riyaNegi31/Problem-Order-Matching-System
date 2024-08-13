8e01a6993953412ffb600668e6dfb27e46a7d60c
riyaNegi31 <106332012+riyaNegi31@users.noreply.github.com>
2024-08-13 11:47:34 +0530

    Delete Problem-Order-Matching-System-master directory

--- Problem-Order-Matching-System-master/task/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import orderReducer from "./slices/ordersSlice";
-const store = configureStore({
-  reducer: {
-    orders: orderReducer,
-  },
-});
-export default store;
